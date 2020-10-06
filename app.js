010aaf91ce708c25aa541b1164a939923623ec3a
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-06 12:36:41 +0900

    クイズ機能完成

--- app.js
+++ app.js
@@ -1,26 +1,76 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？'
-const answers = [
-  "スーパーファミコン",
-  "プレーステーション２",
-  "ニンテンドースイッチ",
-  "ニンテンドーDS"
+const quiz = [
+  {
+    question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+    answers: [
+        "スーパーファミコン",
+      "プレーステーション２",
+      "ニンテンドースイッチ",
+      "ニンテンドーDS" 
+    ],
+    correct: 'ニンテンドーDS'
+  },{
+    question: '3+3は？',
+    answers: [
+      "1",
+      "2",
+      "3",
+      "6"
+    ],
+    correct: "6"
+  },{
+    question: '5+5は？',
+    answers: [
+      "1",
+      "2",
+      "3",
+      "10"
+    ],
+    correct: '10'
+  }
 ];
-const correct = 'ニンテンドーDS';
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
-console.log();
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-document.getElementById('js-question').textContent = question;
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  let buttonIndex = 0;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setupQuiz();
 
-for(let i = 0;i < answers.length;i++){
-  document.getElementsByTagName('button')[i].textContent = answers[i];
-  document.getElementsByTagName('button')[i].addEventListener('click', () =>{
-    if(correct === document.getElementsByTagName('button')[i].textContent) {
-      window.alert('正解!');
-    }else{
-      window.alert('不正解!');
-    }
-  })
+const clickHandler = (e) => {
+  if(quiz[quizIndex].correct === e.target.textContent){
+    window.alert('正解');
+    score++;
+  }else{
+    window.alert('不正解');
+  }
+
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+   setupQuiz();
+  }else{
+    window.alert(`終了！あなたの正解数は${score}/ ${quizLength}です`)
+  }
 };
+
+let handlerIndex = 0;
+while (handlerIndex < buttonLength){
+  $button[handlerIndex].addEventListener('click',(e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
+
+
 
 
 
